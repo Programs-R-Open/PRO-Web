5c053f5847249236c5168cb5289ae1bc1bdb4103
Alvaro <alvaro55cuartero98@gmail.com>
2021-05-18 08:43:33 +0200

    Output compatibility with Unix systems

--- src/Proweb/Output.py
+++ src/Proweb/Output.py
@@ -1,28 +1,51 @@
-STD_INPUT_HANDLE = -10
-STD_OUTPUT_HANDLE= -11
-STD_ERROR_HANDLE = -12
-
-FOREGROUND_BLUE = 0x01 # text color contains blue.
-FOREGROUND_GREEN= 0x02 # text color contains green.
-FOREGROUND_RED  = 0x04 # text color contains red.
-FOREGROUND_INTENSITY = 0x08 # text color is intensified.
-
-FOREGROUND_WHITE = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
-
-
-BACKGROUND_BLUE = 0x10 # background color contains blue.
-BACKGROUND_GREEN= 0x20 # background color contains green.
-BACKGROUND_RED  = 0x40 # background color contains red.
-BACKGROUND_INTENSITY = 0x80 # background color is intensified.
-
-
-
 import ctypes
+import os
 
-std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+if os.name == "nt":
+
+	STD_INPUT_HANDLE = -10
+	STD_OUTPUT_HANDLE= -11
+	STD_ERROR_HANDLE = -12
+
+	FOREGROUND_BLUE = 0x01 # text color contains blue.
+	FOREGROUND_GREEN= 0x02 # text color contains green.
+	FOREGROUND_RED  = 0x04 # text color contains red.
+	FOREGROUND_INTENSITY = 0x08 # text color is intensified.
+
+	FOREGROUND_WHITE = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+
+
+	BACKGROUND_BLUE = 0x10 # background color contains blue.
+	BACKGROUND_GREEN= 0x20 # background color contains green.
+	BACKGROUND_RED  = 0x40 # background color contains red.
+	BACKGROUND_INTENSITY = 0x80 # background color is intensified.
+
+	std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+elif os.name == "posix":
+
+	FOREGROUND_RED = "\x1B[31m";
+	FOREGROUND_GREEN = "\x1B[32m";
+	FOREGROUND_YELLOW = "\x1B[33m";
+	FOREGROUND_BLUE = "\x1B[34m";
+	FOREGROUND_MAGENTA = "\x1B[35m";
+	FOREGROUND_CYAN = "\x1B[36m";
+	FOREGROUND_WHITE = "\x1B[0m";
+
+
+
+	BACKGROUND_BLUE = 0x10 # background color contains blue.
+	BACKGROUND_GREEN= 0x20 # background color contains green.
+	BACKGROUND_RED  = 0x40 # background color contains red.
+	BACKGROUND_INTENSITY = 0x80 # background color is intensified.
+
+
 
 def set_color(color, handle=std_out_handle):
-	bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+	if os.name == "nt":
+		bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+	elif os.name == "posix":
+		print(color);
 	return bool;
 
 def ColorPrint(color, *text):
